543ef7647c088c78346f0117db07d6f8dd8e79c7
Forrest Sutton <forrestsutton@gmail.com>
2016-06-03 10:46:48 -0700

    add package.json

---
+++ components/App.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import ChannelSection from './channels/ChannelSection.jsx';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      channels: []
+    };
+  }
+  addChannel(name){
+    let {channels} = this.state;
+    channels.push({id: channels.length, name});
+    this.setState({channels});
+     //TODO: Send to server
+  }
+  setChannel(activeChannel){
+    this.setState({activeChannel});
+    //TODO: get channels message 
+  }
+  
+  render(){
+	return (
+          <ChannelSection
+	    channels={this.state.channels}
+            addChannel={this.addChannel.bind(this)}
+	    setChannel={this.setChannel.bin(this)}	     	
+	/> 
+       )
+    }  
+}
+
+export default App
